6742ce7fd7778b9f64e49e3aa0fcb3a66dbc6c4f
Justin Léger <juslleg@gmail.com>
2017-10-09 13:52:18 -0400

    Update updateYourDatePLZ.js

--- updateYourDatePLZ.js
+++ updateYourDatePLZ.js
@@ -1,4 +1,3 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var date = new Date().getFullYear();
-    document.getElementById("updateYourYearPLZ").innerHTML = date;
+    document.getElementById("updateYourYearPLZ").innerHTML = new Date().getFullYear();
 });
